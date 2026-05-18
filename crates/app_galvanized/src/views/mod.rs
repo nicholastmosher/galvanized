b0fce74c52176da8528b111e9bbd36a67ae1a562
Nick Mosher <nicholastmosher@pm.me>
2026-05-18 12:48:14 -0400

    Begin create_profile_item

--- crates/app_galvanized/src/views/mod.rs
+++ crates/app_galvanized/src/views/mod.rs
@@ -1,6 +1,7 @@
 use zed::unstable::ui::App;
 
 pub mod connections;
+pub mod create_profile_item;
 pub mod create_profile_modal;
 pub mod create_space_modal;
 pub mod panel_root;
@@ -8,4 +9,5 @@ pub mod panel_root;
 pub fn init(cx: &mut App) {
     connections::init(cx);
     panel_root::init(cx);
+    create_profile_item::init(cx);
 }
