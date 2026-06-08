d9d9cc059edb0073d2eebcc3835aec29546ee2d8
Nick Mosher <nicholastmosher@pm.me>
2026-06-07 21:15:14 -0400

    Remove unused views

--- crates/app_galvanized/src/views/mod.rs
+++ crates/app_galvanized/src/views/mod.rs
@@ -1,9 +1,6 @@
 use zed::unstable::ui::App;
 
 pub mod connections;
-pub mod create_profile_item;
-// pub mod create_profile_modal;
-// pub mod create_space_modal;
 pub mod panel_root;
 pub mod profile_login;
 pub mod vault_login_item;
@@ -11,6 +8,5 @@ pub mod vault_login_item;
 pub fn init(cx: &mut App) {
     connections::init(cx);
     panel_root::init(cx);
-    // create_profile_item::init(cx);
     vault_login_item::init(cx);
 }
